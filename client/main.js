b08cbfeb242d431a1564bb2acc199b87aefbfdfb
ALEXANDER DITZEND <alexander@ALEXANDERs-iMac.local>
2018-02-08 18:14:37 -0300

    styled

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import { liveChat } from 'meteor/live-chat-meteor-client';
 
 import './main.html';
+// import './css/custom.css';
 liveChat.init('YQ65fH4ZZFY4nBCq6');
 
 console.log('livechat', liveChat.userSessionId);
