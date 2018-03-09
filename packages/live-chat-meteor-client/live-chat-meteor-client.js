e3cfea31b1eb6e83ae899bbfbf62c0a80c127e22
ALEXANDER DITZEND <alexander@ALEXANDERs-iMac.local>
2018-03-09 12:04:08 -0300

    scroll

--- packages/live-chat-meteor-client/live-chat-meteor-client.js
+++ packages/live-chat-meteor-client/live-chat-meteor-client.js
@@ -10,9 +10,29 @@ Template.liveChatBox.onRendered(function () {
     const input = tmpl.$('.js-chat-submit-input');
     const data = Template.currentData();
     const userSessionId = data && data.userSessionId;
+    let lastCount = 0;
+    
     tmpl.autorun(() => {
         if (liveChat.messages.count()) {
+
             tmpl.liveChatMessages.set(liveChat.messages);
+
+            if (liveChat.messages.count() > lastCount) {
+                lastCount++;
+                console.log('messages :', lastCount);
+                let h = 0;
+
+                $(".message").each(function (i, val) {
+                    h += parseInt($(this).height());
+                });
+                h += "";
+                console.log("total height ", h);
+                $("#live-chat-messages").animate({ scrollTop: h }, "slow");
+
+            }
+            
+           
+            
         }
     });
 });
@@ -25,6 +45,16 @@ Template.liveChatBox.events({
         const key = e.keyCode || e.which;
         if (key === 13 && !e.shiftKey) {
             e.preventDefault();
+            $(".message").each(function(i,val){
+                h += parseInt($(this).height());
+            });
+            h += 80;
+            h += "";
+
+            // $("#live-chat-messages").scrollTop($("#live-chat-messages")[0].scrollHeight - 250);
+            // let divHeight = $("#live-chat-messages").height();
+            // console.log('DIV HEIGHT', divHeight);
+            // $("#live-chat-item").animate({scrollTop: $("#chat-end")}, "slow");
             if (msg.trim() !== '') {
                 // liveChat.connection.send(msg);
                 liveChat.ddp.call('addIncomingMsg', msg, clientAppId, userSessionId);
