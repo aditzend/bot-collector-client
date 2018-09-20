2aee754982ef46ad7da57cd2db6df0a023bf26a9
Alexander Ditzend <alexander.ditzend@mitrol.net>
2018-09-20 18:25:22 -0300

    anda react

--- client/main.js
+++ client/main.js
@@ -1,6 +1,8 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { liveChat } from 'meteor/live-chat-meteor-client';
+import React from 'react'
+import { render } from 'react-dom'
 
 import './main.html';
 // import './charts.js'
@@ -9,3 +11,7 @@ liveChat.init('iWZYqELkHENCz76Sb');
 
 console.log('userSessionId', liveChat.userSessionId);
 console.log('clientAppId', liveChat.clientAppId);
+
+Meteor.startup( () => {
+    render(<h1>hello react</h1>, document.getElementById('react-container'))
+})
