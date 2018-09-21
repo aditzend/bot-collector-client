69e8fd0c58a96aeb515a3f8b8af9ef1beea7a724
Alexander Ditzend <alexander.ditzend@mitrol.net>
2018-09-21 13:32:40 -0300

    charts reactivos

--- client/Chart.js
+++ client/Chart.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
-import { LineChart, Line } from 'recharts'
+import { LineChart,
+         Line,
+        CartesianGrid,
+        AreaChart,
+        Area,
+        PieChart,
+        Pie,
+        Legend
+     } from 'recharts'
 import { withTracker } from 'meteor/react-meteor-data'
 import {
     liveChat
 } from 'meteor/live-chat-meteor-client';
+import XAxis from 'recharts/lib/cartesian/XAxis';
+import YAxis from 'recharts/lib/cartesian/YAxis';
+import moment from 'moment'
+import Tooltip from 'recharts/lib/component/Tooltip';
+import { emit } from 'cluster';
 
 class Chart extends Component {
     getExpenses() {
@@ -32,17 +45,56 @@ class Chart extends Component {
        
         return(
             <div>
-                <ul>{this.renderExpenses()}</ul>
-                <LineChart width={400} height={400} data={this.props.expenses}>
-                    <Line type="monotone" dataKey="cost" stroke="#8884d7" />
-                </LineChart>
+                <AreaChart width={730} height={250} data={this.props.expenses}>
+                    <defs>
+                        <linearGradient id="colorUser1" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                        </linearGradient>
+                        <linearGradient id="colorUser2" x1="0" y1="0" x2="0" y2="1">
+                            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                        </linearGradient>
+                    </defs>
+                    <XAxis dataKey="date" />
+                    <YAxis />
+                    <CartesianGrid strokeDashArray="9 9" />
+                    <Tooltip />
+                    <Area type="monotone" dataKey="cost" stroke="#8884d8" fillOpacity={1} fill="url(#colorUser1)" />
+                    <Area type="monotone" dataKey="double" stroke="#82ca9d" fillOpacity={1} fill="url(#colorUser2)" />
+                </AreaChart>
+                <h3>Gastos por concepto</h3>
+                <PieChart width={730} height={250}>
+                    <Pie data={this.props.sum} dataKey="cost" nameKey="concept" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                    <Legend />
+                    <Tooltip />
+                </PieChart>
             </div>
         )
     }
 }
 
 export default withTracker(() => {
+    let agg = {Gimnasio:0, Super:0}
+    liveChat.expenses.forEach( (e) => {
+        agg[e.concept]++
+    }
+
+    )
     return {
-        expenses: liveChat.expenses.fetch()
+        expenses: liveChat.expenses.map( function(e) {
+            return {
+                cost: e.cost,
+                double: e.cost * 2,
+                concept: e.concept,
+                date: moment(e.date).format("HH:mm")
+            }
+        }),
+        sum: [
+            { concept: "Gimnasio", cost: agg["Gimnasio"]},
+            { concept: "Super", cost: agg["Super"]},
+        ]
+          
+        
     }
 }) (Chart)
